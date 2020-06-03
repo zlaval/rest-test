08077548e9b129d4874b2314f090539a75e9a75f
zlaval <zlaval@gmail.com>
2020-06-04 01:42:17 +0200

    Post endpoint added

--- src/main/kotlin/com/zlrx/container/resttest/Start.kt
+++ src/main/kotlin/com/zlrx/container/resttest/Start.kt
@@ -1,13 +1,28 @@
 package com.zlrx.container.resttest
 
+import com.google.gson.Gson
+import spark.ResponseTransformer
 import spark.kotlin.ignite
 
 
 fun main() {
+
     val http = ignite()
     println("Server is up. Port 4567")
     http.get("/") {
         "OK"
     }
 
+    http.post("/", "application/json") {
+        response.header("Content-Type", "application/json")
+        request.body()
+    }
+
+}
+
+class JsonTransformer : ResponseTransformer {
+    private val gson = Gson()
+    override fun render(model: Any?): String {
+        return gson.toJson(model)
+    }
 }
